4cfa85b9e873a0679172bcd8cbdb7d8e3d645fd5
artursem <artur.semeniuk@gmail.com>
2021-09-06 20:07:30 +0200

    update files structure

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import "./Expenses.css";
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseItem from "./ExpenseItem";
 
-function Expenses(props) {
+const Expenses = (props) =>  {
    
     return (
 		<Card className="expenses">
