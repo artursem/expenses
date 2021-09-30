3bbda59078f757fc1463a0b8b817b47cbdd91320
artursem <artur.semeniuk@gmail.com>
2021-09-30 19:26:22 +0200

    add expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import "./Expenses.css";
-import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
 
 const Expenses = (props) => {
 	const [filteredYear, setFilteredYear] = useState("2020");
@@ -15,19 +15,6 @@ const Expenses = (props) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 
-	let expensesContent = <p>No expenses found.</p>;
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map((expense) => (
-			<ExpenseItem
-				key={expense.id}
-				title={expense.title}
-				amount={expense.amount}
-				date={expense.date}
-			/>
-		));
-	}
-
 	return (
 		<div>
 			<Card className="expenses">
@@ -35,7 +22,7 @@ const Expenses = (props) => {
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{expensesContent}
+				<ExpensesList items={filteredExpenses}/>
 			</Card>
 		</div>
 	);
